4964fae71a2409f7e7324f451118f891abbc0588
Md Ibraheem Siddiqui <ibraheemsiddiqui0809@gmail.com>
2025-07-18 11:49:00 +0530

    Added search bar component and updated styling

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,25 +1,3 @@
-/* import React from "react";
-
-const Navbar = () => {
-  return (
-    <div className="w-full flex justify-between h-15 items-center bg-gray-200 shadow px-5">
-      <div className="w-[10%] h-full flex items-center">
-        <h1 className="font-bold text-zinc-800">bSTORE</h1>
-      </div>
-      <div className="w-[50%] h-full">
-        <ul className="w-full h-full flex gap-6 list-none items-center text-zinc-800 font-medium">
-          <li className="cursor-pointer">HOME</li>
-          <li className="cursor-pointer">ABOUT</li>
-          <li className="cursor-pointer">CONTACT</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-export default Navbar;
- */
-
 import React from "react";
 
 const Navbar = () => {
